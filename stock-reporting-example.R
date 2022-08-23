9cf64466b103568896b2836b1f42e60f4a33f925
Brandon Farr <bfarr@congress-asset.com>
2022-08-23 11:23:33 -0400

    initial commit

---
+++ stock-reporting-example.R
@@ -0,0 +1,12 @@
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(tidyquant))
+library(readxl)
+library(lubridate)
+
+stock_tbl <- tq_get(
+  c("AMZN", "AAPL", "NVDA", "GOOG"),
+  get = "stock.prices",
+  from = "2019-01-01",
+  to = "2022-06-30"
+)
+
